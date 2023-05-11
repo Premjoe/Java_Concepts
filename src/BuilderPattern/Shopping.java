5dfe8a1014276661f63d404105687f4803028c01
Prem <prem@Prems-Air.fritz.box>
2023-05-11 22:47:48 +0200

    Added the Builder Pattern

---
+++ src/BuilderPattern/Shopping.java
@@ -0,0 +1,54 @@
+package BuilderPattern;
+
+public class Shopping {
+	
+//	public Shopping login(int l) {
+//		System.out.println("user is logged in");
+//		return new Shopping();  -- Its not a proper way because for single user passing the object wasting the memory (every time calling the method )
+//	}
+	
+	public Shopping login() {
+		System.out.println("user is logged in");
+		return this; // This means - indicating current class object 
+	}
+
+	public Shopping login(String un, String pwd) {
+		System.out.println("login with " + un + ":" + pwd);
+		return this;
+	}
+
+	public Shopping search(String productName) {
+		System.out.println("search product: " + productName);
+		return this;
+	}
+
+	public Shopping search(String productName, int price) {
+		System.out.println("search product: " + productName + " : " + price);
+		return this;
+	}
+
+	public Shopping addToCart(String productName) {
+		System.out.println("add to cart : " + productName);
+		return this;
+	}
+
+	public Shopping doPayment(String cc, int cvv) {
+		System.out.println("make payment: " + cc + " : " + cvv);
+		return this;
+	}
+
+	public Shopping doPayment(String upi) {
+		System.out.println("make payment: " + upi);
+		return this;
+	}
+
+	public Shopping generateOrder() {
+		System.out.println("generating order, order id is : " + 12345);
+		return this;
+	}
+
+	public Shopping logout() {
+		System.out.println("logout");
+		return this;
+	}
+}
